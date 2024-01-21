83cf11e43d45ee0d50af2992700f997a9a8066e7
Alyssa Riceman <alyssariceman@gmail.com>
2024-01-21 11:47:05 -0500

    refactor session-handling

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
         read_to_string,
         write,
     },
+    
     path::{
         Path,
         PathBuf,
@@ -12,6 +13,7 @@ use std::{
 
 use argh::FromArgs;
 use directories::ProjectDirs;
+use futures::future::join_all;
 use matrix_sdk::{
     Client,
     SessionMeta,
@@ -109,22 +111,15 @@ impl SessionsFile {
 /// Trace Matrix downloader client
 struct Args {
     #[argh(subcommand)]
-    subcommand: Subcommand,
+    subcommand: RootSubcommand,
 }
 
 #[derive(FromArgs)]
 #[argh(subcommand)]
-enum Subcommand {
-    ListLogins(ListLogins),
+enum RootSubcommand {
     ListRooms(ListRooms),
-    Login(Login),
-    Logout(Logout),
+    Session(SessionCommand),
 }
-
-#[derive(FromArgs)]
-#[argh(subcommand, name = "list-logins")]
-/// List currently-logged-in accounts
-struct ListLogins {}
 
 #[derive(FromArgs)]
 #[argh(subcommand, name = "list-rooms")]
@@ -136,15 +131,34 @@ struct ListRooms {
 }
 
 #[derive(FromArgs)]
+#[argh(subcommand, name = "session")]
+/// Add, remove, list, or modify sessions
+struct SessionCommand {
+    #[argh(subcommand)]
+    subcommand: SessionSubcommand,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum SessionSubcommand {
+    List(SessionList),
+    Login(SessionLogin),
+    Logout(SessionLogout),
+    Rename(SessionRename),
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "list")]
+/// List currently-logged-in accounts
+struct SessionList {}
+
+#[derive(FromArgs)]
 #[argh(subcommand, name = "login")]
 /// Log in a new account, creating a new session
-struct Login {
+struct SessionLogin {
     #[argh(positional)]
     /// user id (of the form @alice:example.com) to be logged in
     user_id: String,
-    // #[argh(positional)]
-    // /// password associated with user_id
-    // password: String,
     #[argh(positional)]
     /// optional session name for use in place of the default randomized one
     session_name: Option<String>
@@ -153,18 +167,39 @@ struct Login {
 #[derive(FromArgs)]
 #[argh(subcommand, name = "logout")]
 /// Log out a previously-logged-in account
-struct Logout {
+struct SessionLogout {
     #[argh(positional)]
     /// user id (of the form @alice:example.com) to be logged out
     user_id: String,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "rename")]
+/// Rename a logged-in session
+struct SessionRename {
+    #[argh(positional)]
+    /// user id (of the form @alice:example.com) to be renamed
+    user_id: String,
+    #[argh(positional)]
+    /// new name for session
+    session_name: String,
 }
 
 /////////////////
 //   Helpers   //
 /////////////////
 
+fn add_at_to_user_id_if_applicable(user_id: &str) -> String {
+    if user_id.starts_with('@') {
+        String::from(user_id)
+    } else {
+        format!("@{}", user_id)
+    }
+}
+
 async fn nonfirst_login(user_id: &str, sessions_file: &SessionsFile) -> anyhow::Result<Client> {
-    let session = sessions_file.get(user_id).unwrap();
+    let normalized_user_id = add_at_to_user_id_if_applicable(user_id);
+    let session = sessions_file.get(&normalized_user_id).unwrap();
     let user = UserId::parse(&session.user_id)?;
     let client = Client::builder().server_name(user.server_name()).build().await?;
     client.matrix_auth().restore_session(MatrixSession {
@@ -185,14 +220,6 @@ async fn nonfirst_login(user_id: &str, sessions_file: &SessionsFile) -> anyhow::
 //   Main   //
 //////////////
 
-fn list_logins(sessions_file: &SessionsFile) {
-    // In the long run, replace this with something properly structured with the retrieval and display factored apart from one another
-    println!("Currently-logged-in sessions:");
-    for session in &sessions_file.sessions {
-        println!("{}", session.user_id)
-    }
-}
-
 async fn list_rooms(config: ListRooms, sessions_file: &SessionsFile) -> anyhow::Result<()> {
     // In the long run, replace this with something properly structured with the retrieval and display factored apart from one another
     let client = nonfirst_login(&config.user_id, sessions_file).await?;
@@ -203,12 +230,33 @@ async fn list_rooms(config: ListRooms, sessions_file: &SessionsFile) -> anyhow::
     Ok(())
 }
 
-async fn first_login(config: Login, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
-    if let Ok(_) = sessions_file.get(&config.user_id) {
-        panic!("Tried to log into account {}, but you were already logged in to this account.", &config.user_id); // Replace this with real error-handling.
+async fn session_list(sessions_file: &SessionsFile) -> anyhow::Result<()> {
+    // In the long run, replace this with something properly structured with the retrieval and display factored apart from one another
+    if sessions_file.sessions.len() > 0 {
+        let session_info_to_print = join_all(sessions_file.sessions.iter().map(|session| async {
+            let client = nonfirst_login(&session.user_id, sessions_file).await?;
+            let device_list = client.devices().await?.devices;
+            let device_name = device_list.into_iter().find(|device| device.device_id == session.device_id).unwrap().display_name.unwrap_or_else(|| String::from("[Unnamed]"));
+            anyhow::Result::<(&str, String)>::Ok((&session.user_id, device_name))
+        })).await.into_iter().collect::<anyhow::Result<Vec<(&str, String)>, _>>()?;
+        println!("Currently-logged-in sessions:");
+        for (user_id, session_name) in session_info_to_print {
+            println!("{} | {}", user_id, session_name) // Replace with properly-justified table-formatting in the future
+        }
+    } else {
+        println!("You have no sessions currently logged in.");
     }
 
-    println!("Please input password for account {}.", &config.user_id);
+    Ok(())
+}
+
+async fn session_login(config: SessionLogin, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
+    let normalized_user_id = add_at_to_user_id_if_applicable(&config.user_id);
+    if let Ok(_) = sessions_file.get(&normalized_user_id) {
+        panic!("Tried to log into account {}, but you were already logged into this account.", &normalized_user_id); // Replace this with real error-handling.
+    }
+
+    println!("Please input password for account {}.", &normalized_user_id);
     let password = read_password().unwrap();
 
     let session_name = match config.session_name {
@@ -216,7 +264,7 @@ async fn first_login(config: Login, sessions_file: &mut SessionsFile) -> anyhow:
         None => format!("Trace (Session UUID: {})", Uuid::new_v4())
     };
 
-    let user = UserId::parse(&config.user_id)?;
+    let user = UserId::parse(&normalized_user_id)?;
     let client = Client::builder().server_name(user.server_name()).build().await?;
 
     let login_result = client.matrix_auth().login_username(user, &password).initial_device_display_name(&session_name).send().await?;
@@ -232,10 +280,17 @@ async fn first_login(config: Login, sessions_file: &mut SessionsFile) -> anyhow:
     Ok(())
 }
 
-async fn logout(config: Logout, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
+async fn session_logout(config: SessionLogout, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
     let client = nonfirst_login(&config.user_id, sessions_file).await?;
     client.matrix_auth().logout().await?;
     sessions_file.delete_session(&config.user_id).unwrap();
+
+    Ok(())
+}
+
+async fn session_rename(config: SessionRename, sessions_file: &SessionsFile) -> anyhow::Result<()> {
+    let client = nonfirst_login(&config.user_id, sessions_file).await?;
+    client.rename_device(client.device_id().unwrap(), &config.session_name).await?;
 
     Ok(())
 }
@@ -247,13 +302,14 @@ async fn main() -> anyhow::Result<()> {
 
     let args: Args = argh::from_env();
     match args.subcommand {
-        Subcommand::ListLogins(_l) => list_logins(&sessions_file),
-        Subcommand::ListRooms(l) => list_rooms(l, &sessions_file).await?,
-        Subcommand::Login(l) => first_login(l, &mut sessions_file).await?,
-        Subcommand::Logout(l) => logout(l, &mut sessions_file).await?,
+        RootSubcommand::ListRooms(config) => list_rooms(config, &sessions_file).await?,
+        RootSubcommand::Session(s) => match s.subcommand {
+            SessionSubcommand::List(_) => session_list(&sessions_file).await?,
+            SessionSubcommand::Login(config) => session_login(config, &mut sessions_file).await?,
+            SessionSubcommand::Logout(config) => session_logout(config, &mut sessions_file).await?,
+            SessionSubcommand::Rename(config) => session_rename(config, &sessions_file).await?,
+        }
     };
-
-    // client_auth.logout().await?;
 
     Ok(())
 }
