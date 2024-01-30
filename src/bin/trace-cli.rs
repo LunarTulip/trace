9b3f285e0917fc9393f3cfd948f20d71c291a46e
Alyssa Riceman <alyssariceman@gmail.com>
2024-01-30 13:27:09 -0500

    factor shared functionality back into lib.rs

--- src/bin/trace-cli.rs
+++ src/bin/trace-cli.rs
@@ -1,27 +1,22 @@
-use std::fs::write;
 use std::path::Path;
 
-use trace::*;
+use trace::{
+    SessionsFile,
+    add_at_to_user_id_if_applicable,
+    nonfirst_login,
+};
 
 use argh::FromArgs;
 use directories::ProjectDirs;
-use futures::future::join_all;
 use matrix_sdk::{
     config::SyncSettings,
-    room::MessagesOptions,
     ruma::{
-        events::{
-            room::message::MessageType,
-            AnyMessageLikeEvent,
-            AnyTimelineEvent,
-        },
         presence::PresenceState,
         UserId,
     },
     Client,
 };
 use rpassword::read_password;
-use uuid::Uuid;
 
 //////////////
 //   Args   //
@@ -123,63 +118,20 @@ struct SessionRename {
 //////////////
 
 async fn export(config: Export, sessions_file: &SessionsFile) -> anyhow::Result<()> {
-    // Allow setting export destination other than "directly where run"
     let client = nonfirst_login(&config.user_id, sessions_file).await?;
     client.sync_once(SyncSettings::new().set_presence(PresenceState::Offline)).await?;
-
-    let accessible_rooms_info = get_rooms_info(&client).await?; // This should be possible to optimize out for request-piles without names included, given client.resolve_room_alias and client.get_room. Although that might end up actually costlier if handled indelicately, since it'll involve more serial processing.
-
-    for room_identifier in config.rooms {
-        let room_to_export_info = match get_room_index_by_identifier(&accessible_rooms_info, &room_identifier) {
-            Ok(index) => &accessible_rooms_info[index],
-            Err(e) => match e {
-                RoomIndexRetrievalError::MultipleRoomsWithSpecifiedName(room_ids) => {
-                    println!("Found more than one room accessible to {} with name {}. Room IDs: {:?}", config.user_id, room_identifier, room_ids);
-                    continue
-                },
-                RoomIndexRetrievalError::NoRoomsWithSpecifiedName => {
-                    println!("Couldn't find any rooms accessible to {} with name {}.", config.user_id, room_identifier);
-                    continue
-                },
-            }
-        };
-        let messages = room_to_export_info.room.messages(MessagesOptions::forward()).await?; // Could async this better; try that at some point. Also, looks like for now this is going to get only the first 10 messages?
-        let mut room_export = String::new();
-        for event in messages.chunk {
-            let event_deserialized = event.event.deserialize().unwrap(); // Add real error-handling in place of this unwrap
-            let event_sender_id = event_deserialized.sender();
-            let event_sender_string_representation = match room_to_export_info.room.get_member_no_sync(event_sender_id).await? {
-                Some(room_member) => match room_member.display_name() {
-                    Some(display_name) => format!("{} ({})", display_name, event_sender_id),
-                    None => event_sender_id.to_string(),
-                }
-                None => event_sender_id.to_string(),
-            };
-            let event_stringified = match &event_deserialized {
-                AnyTimelineEvent::MessageLike(e) => match e {
-                    AnyMessageLikeEvent::RoomMessage(e) => match &e.as_original().unwrap().content.msgtype { // Add real handling for the case where the event is redacted
-                        MessageType::Text(e) => format!("{}: {}", event_sender_string_representation, &e.body), // Add timestamps here, and also handling for formatted messages
-                        _ => String::from("[Placeholder message]"),
-                    }
-                    _ => String::from("[Placeholder message-like]"),
-                },
-                AnyTimelineEvent::State(_e) => String::from("[Placeholder state-like]"),
-            };
-            // Add real handling here; this is unreadable, right now
-            room_export.push_str(&format!("{}\n", event_stringified))
-        }
-        write(format!("{}.txt", format_export_filename(&room_to_export_info)), room_export).unwrap(); // Ideally let users pass format strings of some sort here
-    }
+    trace::export(&client, config.rooms).await?;
 
     Ok(())
 }
 
 async fn list_rooms(config: ListRooms, sessions_file: &SessionsFile) -> anyhow::Result<()> {
-    let client = nonfirst_login(&config.user_id, sessions_file).await?;
+    let normalized_user_id = add_at_to_user_id_if_applicable(&config.user_id);
+    let client = nonfirst_login(&normalized_user_id, sessions_file).await?;
     client.sync_once(SyncSettings::new().set_presence(PresenceState::Offline)).await?;
 
-    let rooms_info = get_rooms_info(&client).await?;
-    println!("Rooms joined by {}:", config.user_id);
+    let rooms_info = trace::get_rooms_info(&client).await?;
+    println!("Rooms joined by {}:", normalized_user_id);
     for room_info in rooms_info {
         let room_name = match room_info.name {
             Some(name) => name,
@@ -197,17 +149,10 @@ async fn list_rooms(config: ListRooms, sessions_file: &SessionsFile) -> anyhow::
 }
 
 async fn session_list(sessions_file: &SessionsFile) -> anyhow::Result<()> {
-    if sessions_file.sessions.len() > 0 {
-        let mut session_info_to_print = join_all(sessions_file.sessions.iter().map(|session| async {
-            let client = nonfirst_login(&session.user_id, sessions_file).await?;
-            let device_list = client.devices().await?.devices;
-            let device_name = device_list.into_iter().find(|device| device.device_id == session.device_id).unwrap().display_name.unwrap_or_else(|| String::from("[Unnamed]"));
-            anyhow::Result::<(&str, String)>::Ok((&session.user_id, device_name))
-        })).await.into_iter().collect::<anyhow::Result<Vec<(&str, String)>, _>>()?;
-        session_info_to_print.sort_by_key(|(user_id, _display_name)| *user_id);
-
+    let sessions = trace::list_sessions(sessions_file).await?;
+    if sessions.len() > 0 {
         println!("Currently-logged-in sessions:");
-        for (user_id, session_name) in session_info_to_print {
+        for (user_id, session_name) in sessions {
             println!("{} | {}", user_id, session_name) // Replace with properly-justified table-formatting in the future
         }
     } else {
@@ -220,44 +165,30 @@ async fn session_list(sessions_file: &SessionsFile) -> anyhow::Result<()> {
 async fn session_login(config: SessionLogin, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
     let normalized_user_id = add_at_to_user_id_if_applicable(&config.user_id);
     if let Ok(_) = sessions_file.get(&normalized_user_id) {
-        panic!("Tried to log into account {}, but you were already logged into this account.", &normalized_user_id); // Replace this with real error-handling.
+        panic!("Tried to log into account {}, but you already have a session logged into this account.", &normalized_user_id); // Replace this with real error-handling.
     }
 
     println!("Please input password for account {}.", &normalized_user_id);
     let password = read_password().unwrap();
 
-    let session_name = match config.session_name {
-        Some(name) => name,
-        None => format!("Trace (Session UUID: {})", Uuid::new_v4())
-    };
-
     let user = UserId::parse(&normalized_user_id)?;
     let client = Client::builder().server_name(user.server_name()).build().await?;
 
-    let login_result = client.matrix_auth().login_username(user, &password).initial_device_display_name(&session_name).send().await?;
-    // Add a branch with SSO support, once I know how that's supposed to work
-
-    sessions_file.new_session(Session {
-        user_id: login_result.user_id.to_string(),
-        device_id: login_result.device_id.to_string(),
-        access_token: login_result.access_token.to_string(),
-        refresh_token: login_result.refresh_token,
-    }).unwrap();
+    trace::first_login(&client, sessions_file, &normalized_user_id, &password, config.session_name).await?;
 
     Ok(())
 }
 
 async fn session_logout(config: SessionLogout, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
     let client = nonfirst_login(&config.user_id, sessions_file).await?;
-    client.matrix_auth().logout().await?;
-    sessions_file.delete_session(&config.user_id).unwrap();
+    trace::logout(&client, sessions_file).await?;
 
     Ok(())
 }
 
 async fn session_rename(config: SessionRename, sessions_file: &SessionsFile) -> anyhow::Result<()> {
     let client = nonfirst_login(&config.user_id, sessions_file).await?;
-    client.rename_device(client.device_id().unwrap(), &config.session_name).await?;
+    trace::rename_session(&client, &config.session_name).await?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,33 @@ use std::{
     path::PathBuf,
 };
 
+use chrono::{DateTime, SecondsFormat};
+use futures::future::join_all;
 use matrix_sdk::{
     matrix_auth::{
         MatrixSession,
         MatrixSessionTokens,
     }, 
-    room::Room,
+    room::MessagesOptions,
     ruma::{
+        events::{
+            room::message::MessageType,
+            AnyMessageLikeEvent,
+            AnyTimelineEvent,
+        },
         OwnedRoomAliasId,
         OwnedRoomId,
         UserId,
     },
-    Client, 
+    Client,
+    Room,
     SessionMeta,
 };
 use serde::{
     Deserialize,
     Serialize,
 };
+use uuid::Uuid;
 
 ///////////////////////
 //   Non-arg types   //
@@ -109,9 +118,40 @@ pub enum RoomIndexRetrievalError {
     NoRoomsWithSpecifiedName,
 }
 
-/////////////////
-//   Helpers   //
-/////////////////
+//////////////////////////
+//   Unshared helpers   //
+//////////////////////////
+
+fn get_room_index_by_identifier(rooms_info: &Vec<RoomWithCachedInfo>, identifier: &str) -> Result<usize, RoomIndexRetrievalError> {
+    if let Some(index) = rooms_info.iter().position(|room_info| &room_info.id == identifier) {
+        Ok(index)
+    } else if let Some(index) = rooms_info.iter().position(|room_info| room_info.canonical_alias.as_ref().is_some_and(|alias| alias == identifier)) {
+        Ok(index)
+    } else if let Some(index) = rooms_info.iter().position(|room_info| room_info.alt_aliases.iter().any(|alias| alias == identifier)) {
+        Ok(index)
+    } else {
+        let name_matches = rooms_info.iter().filter(|room_info| room_info.name.as_ref().is_some_and(|name| name == identifier)).collect::<Vec<&RoomWithCachedInfo>>();
+        match name_matches.len() {
+            0 => Err(RoomIndexRetrievalError::NoRoomsWithSpecifiedName),
+            1 => Ok(rooms_info.iter().position(|room_info| room_info.name.as_ref().is_some_and(|name| name  == identifier)).unwrap()),
+            _ => Err(RoomIndexRetrievalError::MultipleRoomsWithSpecifiedName(name_matches.iter().map(|room_info| room_info.id.to_string()).collect())),
+        }
+    }
+}
+
+fn format_export_filename(room_info: &RoomWithCachedInfo) -> String {
+    let (nonserver_id_component, server) = room_info.id.as_str().split_once(':').unwrap();
+    match (&room_info.name, &room_info.canonical_alias) {
+        (Some(name), Some(alias)) => format!("{} [{}, {}, {}]", name, alias.as_str().split_once(':').unwrap().0, nonserver_id_component, server),
+        (Some(name), None) => format!("{} [{}, {}]", name, nonserver_id_component, server),
+        (None, Some(alias)) => format!("{} [{}, {}]", alias.as_str().split_once(':').unwrap().0, nonserver_id_component, server),
+        (None, None) => format!("{} [{}]", nonserver_id_component, server),
+    }
+}
+
+////////////////////////
+//   Shared helpers   //
+////////////////////////
 
 pub fn add_at_to_user_id_if_applicable(user_id: &str) -> String {
     if user_id.starts_with('@') {
@@ -140,6 +180,54 @@ pub async fn nonfirst_login(user_id: &str, sessions_file: &SessionsFile) -> anyh
     Ok(client)
 }
 
+//////////////////////////
+//   Shared functions   //
+//////////////////////////
+
+pub async fn first_login(client: &Client, sessions_file: &mut SessionsFile, user_id: &str, password: &str, session_name: Option<String>) -> anyhow::Result<()> {
+    let session_name = match session_name {
+        Some(name) => name,
+        None => format!("Trace (Session UUID: {})", Uuid::new_v4())
+    };
+
+    let login_result = client.matrix_auth().login_username(user_id, password).initial_device_display_name(&session_name).send().await?;
+    // Add a branch with SSO support, once I know how that's supposed to work
+
+    sessions_file.new_session(Session {
+        user_id: login_result.user_id.to_string(),
+        device_id: login_result.device_id.to_string(),
+        access_token: login_result.access_token.to_string(),
+        refresh_token: login_result.refresh_token,
+    }).unwrap();
+
+    Ok(())
+}
+
+pub async fn logout(client: &Client, sessions_file: &mut SessionsFile) -> anyhow::Result<()> {
+    client.matrix_auth().logout().await?;
+    sessions_file.delete_session(&client.user_id().unwrap().to_string()).unwrap();
+
+    Ok(())
+}
+
+pub async fn list_sessions(sessions_file: &SessionsFile) -> anyhow::Result<Vec<(String, String)>> {
+    let mut sessions_info = join_all(sessions_file.sessions.iter().map(|session| async {
+        let client = nonfirst_login(&session.user_id, sessions_file).await?;
+        let device_list = client.devices().await?.devices;
+        let device_name = device_list.into_iter().find(|device| device.device_id == session.device_id).unwrap().display_name.unwrap_or_else(|| String::from("[Unnamed]"));
+        anyhow::Result::<(String, String)>::Ok((session.user_id.clone(), device_name))
+    })).await.into_iter().collect::<anyhow::Result<Vec<(String, String)>, _>>()?;
+    sessions_info.sort_by(|(user_id_1, _display_name_1), (user_id_2, _display_name_2)| user_id_1.cmp(user_id_2)); // sort_by_key doesn't work here for weird lifetime reasons
+
+    Ok(sessions_info)
+}
+
+pub async fn rename_session(client: &Client, new_session_name: &str) -> anyhow::Result<()> {
+    client.rename_device(client.device_id().unwrap(), new_session_name).await?;
+
+    Ok(())
+}
+
 pub async fn get_rooms_info(client: &Client) -> anyhow::Result<Vec<RoomWithCachedInfo>> {
     let mut rooms_info = client.joined_rooms().into_iter().map(|room| RoomWithCachedInfo {
         id: room.room_id().to_owned(),
@@ -163,29 +251,54 @@ pub async fn get_rooms_info(client: &Client) -> anyhow::Result<Vec<RoomWithCache
     Ok(rooms_info)
 }
 
-pub fn get_room_index_by_identifier(rooms_info: &Vec<RoomWithCachedInfo>, identifier: &str) -> Result<usize, RoomIndexRetrievalError> {
-    if let Some(index) = rooms_info.iter().position(|room_info| &room_info.id == identifier) {
-        Ok(index)
-    } else if let Some(index) = rooms_info.iter().position(|room_info| room_info.canonical_alias.as_ref().is_some_and(|alias| alias == identifier)) {
-        Ok(index)
-    } else if let Some(index) = rooms_info.iter().position(|room_info| room_info.alt_aliases.iter().any(|alias| alias == identifier)) {
-        Ok(index)
-    } else {
-        let name_matches = rooms_info.iter().filter(|room_info| room_info.name.as_ref().is_some_and(|name| name == identifier)).collect::<Vec<&RoomWithCachedInfo>>();
-        match name_matches.len() {
-            0 => Err(RoomIndexRetrievalError::NoRoomsWithSpecifiedName),
-            1 => Ok(rooms_info.iter().position(|room_info| room_info.name.as_ref().is_some_and(|name| name  == identifier)).unwrap()),
-            _ => Err(RoomIndexRetrievalError::MultipleRoomsWithSpecifiedName(name_matches.iter().map(|room_info| room_info.id.to_string()).collect())),
-        }
-    }
-}
+pub async fn export(client: &Client, rooms: Vec<String>) -> anyhow::Result<()> {
+    // Allow setting export destination other than "directly where run"
+    let accessible_rooms_info = get_rooms_info(&client).await?; // This should be possible to optimize out for request-piles without names included, given client.resolve_room_alias and client.get_room. Although that might end up actually costlier if handled indelicately, since it'll involve more serial processing.
 
-pub fn format_export_filename(room_info: &RoomWithCachedInfo) -> String {
-    let (nonserver_id_component, server) = room_info.id.as_str().split_once(':').unwrap();
-    match (&room_info.name, &room_info.canonical_alias) {
-        (Some(name), Some(alias)) => format!("{} [{}, {}, {}]", name, alias.as_str().split_once(':').unwrap().0, nonserver_id_component, server),
-        (Some(name), None) => format!("{} [{}, {}]", name, nonserver_id_component, server),
-        (None, Some(alias)) => format!("{} [{}, {}]", alias.as_str().split_once(':').unwrap().0, nonserver_id_component, server),
-        (None, None) => format!("{} [{}]", nonserver_id_component, server),
+    for room_identifier in rooms {
+        let room_to_export_info = match get_room_index_by_identifier(&accessible_rooms_info, &room_identifier) {
+            Ok(index) => &accessible_rooms_info[index],
+            Err(e) => match e {
+                // This is currently CLI-biased; modify it to return error-info in a more neutral way
+                RoomIndexRetrievalError::MultipleRoomsWithSpecifiedName(room_ids) => {
+                    println!("Found more than one room accessible to {} with name {}. Room IDs: {:?}", client.user_id().unwrap(), room_identifier, room_ids);
+                    continue
+                },
+                RoomIndexRetrievalError::NoRoomsWithSpecifiedName => {
+                    println!("Couldn't find any rooms accessible to {} with name {}.", client.user_id().unwrap(), room_identifier);
+                    continue
+                },
+            }
+        };
+        let messages = room_to_export_info.room.messages(MessagesOptions::forward()).await?; // Could async this better; try that at some point. Also, looks like for now this is going to get only the first 10 messages?
+        let mut room_export = String::new();
+        for event in messages.chunk {
+            let event_deserialized = event.event.deserialize().unwrap(); // Add real error-handling in place of this unwrap
+            let event_timestamp_millis = event_deserialized.origin_server_ts().0.into();
+            let event_timestamp = DateTime::from_timestamp_millis(event_timestamp_millis).expect(&format!("Found message with millisecond timestamp {}, which can't be converted to datetime.", event_timestamp_millis)); // Add option to use local time zones
+            let event_sender_id = event_deserialized.sender();
+            let event_sender_string_representation = match room_to_export_info.room.get_member_no_sync(event_sender_id).await? {
+                Some(room_member) => match room_member.display_name() {
+                    Some(display_name) => format!("{} ({})", display_name, event_sender_id),
+                    None => event_sender_id.to_string(),
+                }
+                None => event_sender_id.to_string(),
+            };
+            let event_stringified = match &event_deserialized {
+                AnyTimelineEvent::MessageLike(e) => match e {
+                    AnyMessageLikeEvent::RoomMessage(e) => match &e.as_original().unwrap().content.msgtype { // Add real handling for the case where the event is redacted
+                        MessageType::Text(e) => format!("[{}] {}: {}", event_timestamp.to_rfc3339_opts(SecondsFormat::Millis, true), event_sender_string_representation, &e.body), // Add handling for formatted messages
+                        _ => String::from("[Placeholder message]"),
+                    }
+                    _ => String::from("[Placeholder message-like]"),
+                },
+                AnyTimelineEvent::State(_e) => String::from("[Placeholder state-like]"),
+            };
+            // Add real handling here; this is unreadable, right now
+            room_export.push_str(&format!("{}\n", event_stringified))
+        }
+        write(format!("{}.txt", format_export_filename(&room_to_export_info)), room_export).unwrap(); // Ideally let users pass format strings of some sort here
     }
+
+    Ok(())
 }
